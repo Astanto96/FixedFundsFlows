cc57e469685fe21d3c0b7dc640e6ee738eba7ffb
Felix <61760684+Astanto96@users.noreply.github.com>
2025-02-28 15:01:37 +0100

    OverviewRepository

---
+++ lib/data/repositories/overview_repository.dart
@@ -0,0 +1,68 @@
+import 'package:fixedfundsflows/core/utils/billing_period.dart';
+import 'package:fixedfundsflows/data/repositories/contract_repository.dart';
+import 'package:fixedfundsflows/domain/contract.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'overview_repository.g.dart';
+
+@riverpod
+OverviewRepository overviewRepository(Ref ref) {
+  final contractRepo = ref.watch(contractRepositoryProvider);
+  return OverviewRepository(contractRepo);
+}
+
+class OverviewRepository {
+  final ContractRepository contractRepository;
+
+  OverviewRepository(this.contractRepository);
+
+  Future<List<Contract>> getContractsForPeriod(
+    BillingPeriod selectedPeriod,
+  ) async {
+    final contracts = contractRepository.getContracts();
+
+    return contracts.map((contract) {
+      final adjustedAmount = _calculateAmountPerPeriod(
+        contract.amount,
+        contract.billingPeriod,
+        selectedPeriod,
+      );
+      return contract.copyWith(amount: adjustedAmount);
+    }).toList();
+  }
+
+  int _calculateAmountPerPeriod(
+    int amount,
+    BillingPeriod originalPeriod,
+    BillingPeriod targetPeriod,
+  ) {
+    const monthsPerYear = 12;
+    const monthsPerQuarter = 3;
+
+    if (originalPeriod == targetPeriod) return amount;
+
+    if (originalPeriod == BillingPeriod.monthly) {
+      if (targetPeriod == BillingPeriod.quarterly) {
+        return amount * monthsPerQuarter;
+      }
+      if (targetPeriod == BillingPeriod.yearly) return amount * monthsPerYear;
+    } else if (originalPeriod == BillingPeriod.quarterly) {
+      if (targetPeriod == BillingPeriod.monthly) {
+        return (amount / monthsPerQuarter).round();
+      }
+      if (targetPeriod == BillingPeriod.yearly) {
+        return (amount * (monthsPerYear / monthsPerQuarter)).round();
+      }
+    } else if (originalPeriod == BillingPeriod.yearly) {
+      if (targetPeriod == BillingPeriod.monthly) {
+        return (amount / monthsPerYear).round();
+      }
+      if (targetPeriod == BillingPeriod.quarterly) {
+        return (amount / (monthsPerYear / monthsPerQuarter)).round();
+      }
+    }
+
+    throw ArgumentError('Unsupported BillingPeriod conversion');
+  }
+}
---
+++ lib/data/repositories/overview_repository.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'overview_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$overviewRepositoryHash() =>
+    r'22a4fab655e363d727b20076ae8303e7da292a04';
+
+/// See also [overviewRepository].
+@ProviderFor(overviewRepository)
+final overviewRepositoryProvider =
+    AutoDisposeProvider<OverviewRepository>.internal(
+  overviewRepository,
+  name: r'overviewRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$overviewRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef OverviewRepositoryRef = AutoDisposeProviderRef<OverviewRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
