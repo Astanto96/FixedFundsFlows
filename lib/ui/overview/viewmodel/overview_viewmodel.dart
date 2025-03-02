09f46c62c6a7f4ac7832ff5d6aa3bddc4a9f348f
Felix <61760684+Astanto96@users.noreply.github.com>
2025-03-02 15:40:26 +0100

    OverviewViewModel v1

---
+++ lib/ui/overview/viewmodel/overview_viewmodel.dart
@@ -0,0 +1,37 @@
+import 'package:fixedfundsflows/core/utils/billing_period.dart';
+import 'package:fixedfundsflows/data/repositories/overview_repository.dart';
+import 'package:fixedfundsflows/ui/overview/viewmodel/overview_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'overview_viewmodel.g.dart';
+
+//_$OverviewViewModel is a "class annotation" from riverpod generator
+//it returns an Object with State
+@riverpod
+class OverviewViewModel extends _$OverviewViewModel {
+  late final OverviewRepository _repository;
+
+  @override
+  OverviewState build() {
+    _repository = ref.watch(overviewRepositoryProvider);
+    loadContractsForPeriod();
+    return OverviewState();
+  }
+
+  void setBillingPeriod(BillingPeriod period) {
+    state = state.copyWith(selectedPeriod: period);
+    loadContractsForPeriod();
+  }
+
+  Future<void> loadContractsForPeriod() async {
+    state = state.copyWith(isLoading: true);
+
+    try {
+      final contracts =
+          await _repository.getContractsForPeriod(state.selectedPeriod);
+      state = state.copyWith(contracts: contracts, isLoading: false);
+    } catch (e) {
+      state = state.copyWith(isLoading: false, error: e.toString());
+    }
+  }
+}
---
+++ lib/ui/overview/viewmodel/overview_viewmodel.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'overview_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$overviewViewModelHash() => r'51cf4b2a84d9718c0c6ce1eafb23cab6f39326c9';
+
+/// See also [OverviewViewModel].
+@ProviderFor(OverviewViewModel)
+final overviewViewModelProvider =
+    AutoDisposeNotifierProvider<OverviewViewModel, OverviewState>.internal(
+  OverviewViewModel.new,
+  name: r'overviewViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$overviewViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$OverviewViewModel = AutoDisposeNotifier<OverviewState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
