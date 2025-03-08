1737df7d434ab720e6563067598d2385cb4f0752
Felix <61760684+Astanto96@users.noreply.github.com>
2025-03-08 17:15:22 +0100

    fix typi

--- lib/core/theme/light_theme.dart
+++ lib/core/theme/light_theme.dart
@@ -11,34 +11,34 @@ class LightTheme {
 
     final base = ThemeData.light();
     return base.copyWith(
-      colorScheme: base.colorScheme.copyWith(
-        surface: surface,
-        primary: primary,
-        secondary: secondary,
-        inversePrimary: inversePrimary,
-      ),
-      textTheme: base.textTheme.apply(
-        bodyColor: Colors.grey[800],
-        displayColor: Colors.black,
-      ),
-      navigationBarTheme: const NavigationBarThemeData(
-        backgroundColor: surface,
-        indicatorColor: secondary,
-        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-      ),
-      floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        backgroundColor: primary,
-        foregroundColor: inversePrimary,
-      ),
-      bottomAppBarTheme: const BottomAppBarTheme(
-        color: surface,
-        elevation: 2,
-      ),
-      appBarTheme: const AppBarTheme(
-        backgroundColor: primary,
-        foregroundColor: inversePrimary,
-        elevation: 0,
-      ),
-    );
+        colorScheme: base.colorScheme.copyWith(
+          surface: surface,
+          primary: primary,
+          secondary: secondary,
+          inversePrimary: inversePrimary,
+        ),
+        textTheme: base.textTheme.apply(
+          bodyColor: Colors.grey[800],
+          displayColor: Colors.black,
+        ),
+        navigationBarTheme: const NavigationBarThemeData(
+          backgroundColor: surface,
+          indicatorColor: secondary,
+          labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: primary,
+          foregroundColor: inversePrimary,
+        ),
+        bottomAppBarTheme: const BottomAppBarTheme(
+          color: surface,
+          elevation: 2,
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: primary,
+          foregroundColor: inversePrimary,
+          elevation: 0,
+        ),
+        scaffoldBackgroundColor: primary);
   }
 }
