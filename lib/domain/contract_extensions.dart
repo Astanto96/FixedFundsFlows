52d5a92ade99a2443b01c64316a880683c32cbeb
Felix <61760684+Astanto96@users.noreply.github.com>
2025-03-08 18:24:50 +0100

    extension for Contract Domainmodel -> billingIcon

---
+++ lib/domain/contract_extensions.dart
@@ -0,0 +1,18 @@
+import 'package:fixedfundsflows/core/utils/billing_period.dart';
+import 'package:fixedfundsflows/domain/contract.dart';
+import 'package:flutter/material.dart';
+
+extension BillingIconExtension on Contract {
+  IconData get billingIcon {
+    switch (billingPeriod) {
+      case BillingPeriod.monthly:
+        return Icons.calendar_today;
+      case BillingPeriod.quarterly:
+        return Icons.today;
+      case BillingPeriod.yearly:
+        return Icons.calendar_month;
+      default:
+        return Icons.error_outline;
+    }
+  }
+}
