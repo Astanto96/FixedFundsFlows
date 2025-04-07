57bc6356afa5f7e8733ff6592f90be54d3e3debd
Felix <61760684+Astanto96@users.noreply.github.com>
2025-04-07 13:36:31 +0200

    Theme base on Plattformtheme

--- lib/core/theme/theme_provider.dart
+++ lib/core/theme/theme_provider.dart
@@ -8,8 +8,39 @@ part 'theme_provider.g.dart';
 
 @riverpod
 class ThemeNotifier extends _$ThemeNotifier {
+  late final VoidCallback _brightnessListener;
+
   @override
-  ThemeMode build() => ThemeMode.dark;
+  ThemeMode build() {
+    // Chose Theme based on system brightness
+    final brightness =
+        WidgetsBinding.instance.platformDispatcher.platformBrightness;
+    final mode =
+        brightness == Brightness.dark ? ThemeMode.dark : ThemeMode.light;
+
+    // register listener for system brightness changes
+    _brightnessListener = () {
+      final newBrightness =
+          WidgetsBinding.instance.platformDispatcher.platformBrightness;
+      final newMode =
+          newBrightness == Brightness.dark ? ThemeMode.dark : ThemeMode.light;
+
+      if (state != newMode) {
+        state = newMode;
+      }
+    };
+
+    WidgetsBinding.instance.platformDispatcher.onPlatformBrightnessChanged =
+        _brightnessListener;
+
+    ref.onDispose(() {
+      //Unregister the listener when the provider is disposed
+      WidgetsBinding.instance.platformDispatcher.onPlatformBrightnessChanged =
+          null;
+    });
+
+    return mode;
+  }
 
   void toggleTheme() {
     state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
@@ -19,6 +50,5 @@ class ThemeNotifier extends _$ThemeNotifier {
 @riverpod
 ThemeData currentTheme(Ref ref) {
   final mode = ref.watch(themeNotifierProvider);
-
   return mode == ThemeMode.light ? LightTheme.theme : DarkTheme.theme;
 }
