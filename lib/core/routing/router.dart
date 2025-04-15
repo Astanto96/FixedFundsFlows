d3939ebd4111d35c124b8331fea19387639287e4
Felix <61760684+Astanto96@users.noreply.github.com>
2025-04-15 22:00:43 +0200

    added to fix

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,6 +1,6 @@
 import 'package:fixedfundsflows/core/layout/layout_scaffold.dart';
 import 'package:fixedfundsflows/ui/auth/widgets/auth_screen.dart';
-import 'package:fixedfundsflows/ui/categorys/widgets/categories_screen.dart';
+import 'package:fixedfundsflows/ui/categories/widgets/categories_screen.dart';
 import 'package:fixedfundsflows/ui/overview/widgets/overview_screen.dart';
 import 'package:fixedfundsflows/ui/statistic/widgets/statistic_screen.dart';
 import 'package:flutter/material.dart';
@@ -28,7 +28,7 @@ final router = GoRouter(
         StatefulShellBranch(
           routes: [
             GoRoute(
-              path: '/categorys',
+              path: '/categories',
               builder: (context, state) => const CategoriesScreen(),
             ),
           ],
